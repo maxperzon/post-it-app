2706eb1d8a3fec775e7af173e849d8946c46ab5d
maxperzon <maxperzon@gmail.com>
2015-10-05 17:40:09 +0200

    Views

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,14 +1,25 @@
 class QuotesController < ApplicationController
+	before_action :find_quote, only: [:show, :edit, :update, :destroy]
+	
 	def index
+		@quotes = Quote.all.order("created_at DESC")
 	end
 
 	def show
 	end
 
 	def new
+		@quote = Quote.new
 	end
 
 	def create
+		@quote = Quote.new(quote_params)
+
+		if @quote.save
+			redirect_to @quote
+		else
+			render 'new'
+		end
 	end
 
 	def edit
@@ -23,8 +34,10 @@ class QuotesController < ApplicationController
 	private
 
 	def find_quote
+		@quote = Quote.find(params[:id])
 	end
 
 	def quote_params
+		params.require(:quote).permit(:title, :content)
 	end
 end
