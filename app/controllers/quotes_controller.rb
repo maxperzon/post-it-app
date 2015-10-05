6c1f0767080142458d03cd0d6bece099baa2905e
maxperzon <maxperzon@gmail.com>
2015-10-05 15:18:37 +0200

    quotes controller and routes

---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,30 @@
+class QuotesController < ApplicationController
+	def index
+	end
+
+	def show
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+
+	def find_quote
+	end
+
+	def quote_params
+	end
+end
